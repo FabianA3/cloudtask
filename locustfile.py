86c42f7b1ea26d9a0306799de98272c34cebe05b
Fabian andres Ariza <btcfabian217@gmail.com>
2026-09-21 12:58:00 -0500

    Agrega pruebas de carga con Locust

---
+++ locustfile.py
@@ -0,0 +1,10 @@
+from locust import HttpUser, task, between
+
+
+class CloudTaskUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def health_check(self):
+        self.client.get("/health")
+        
